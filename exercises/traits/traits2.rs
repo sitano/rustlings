ddbd48cc53615645a370478305cdf0c647a1717c
Ivan Prisyazhnyy <john.koepi@gmail.com>
2020-08-29 13:04:58 +0200

    threads, traits, vars

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,13 +10,17 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-//TODO: Add your code here
+impl AppendBar for std::vec::Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut m = self.to_vec();
+        m.push(String::from("Bar"));
+        return m;
+    }
+}
 
 #[cfg(test)]
 mod tests {
